3fee55b0431832a96c680e6a3229c4c7d72ed6f4
leonardo maldonado <lrmaldo@gmail.com>
2024-10-07 11:03:39 -0600

    cambio de codigo de eliminacion de archiv

--- app/Http/Controllers/DispositivoController.php
+++ app/Http/Controllers/DispositivoController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Dispositivo;
 use App\Http\Requests\StoreDispositivoRequest;
 use App\Http\Requests\UpdateDispositivoRequest;
-use App\Models\Torre;
+use App\Models\Dispositivo;
 use Illuminate\Http\File;
 use Illuminate\Support\Facades\Storage;
 
@@ -13,7 +12,6 @@ class DispositivoController extends Controller
 {
 
     public $torre_id;
-
 
     /**
      * Display a listing of the resource.
@@ -32,7 +30,7 @@ class DispositivoController extends Controller
      */
     public function create($torre_id)
     {
-        return view('dispositivos.create',compact('torre_id'));
+        return view('dispositivos.create', compact('torre_id'));
     }
 
     /**
@@ -43,30 +41,29 @@ class DispositivoController extends Controller
      */
     public function store(StoreDispositivoRequest $request)
     {
-         /* resquest tipo file  */
-          if($request->hasFile('img_url')){
+        /* resquest tipo file  */
+        if ($request->hasFile('img_url')) {
             $file = $request->file('img_url');
-            $name = time().$file->getClientOriginalName();
-              Storage::disk('public')->put('images/', $file);
-          }
-            $dispositivo = new Dispositivo();
-            $dispositivo->tipo = $request->tipo;
-            $dispositivo->hostname = $request->hostname;
-            $dispositivo->ip = $request->ip;
-            $dispositivo->usuario = $request->usuario;
-            $dispositivo->password = $request->password;
-            $dispositivo->ssid = $request->ssid;
-            $dispositivo->numero_de_cliente = $request->numero_de_cliente;
-            $dispositivo->marca = $request->marca;
-            $dispositivo->modelo = $request->modelo;
-            $dispositivo->latitud = $request->latitud;
-            $dispositivo->longitud = $request->longitud;
-            $dispositivo->img_url = Storage::url('images/'.$name);
-            $dispositivo->comentario = $request->comentario;
-            $dispositivo->torre_id = $request->torre_id;
-            $dispositivo->estado = $request->estado;
-            $dispositivo->save();
-            return redirect()->route('torres.show',$request->torre_id)->with('success','Dispositivo creado correctamente');
+            Storage::disk('public')->put('images/', $file);
+        }
+        $dispositivo = new Dispositivo();
+        $dispositivo->tipo = $request->tipo;
+        $dispositivo->hostname = $request->hostname;
+        $dispositivo->ip = $request->ip;
+        $dispositivo->usuario = $request->usuario;
+        $dispositivo->password = $request->password;
+        $dispositivo->ssid = $request->ssid;
+        $dispositivo->numero_de_cliente = $request->numero_de_cliente;
+        $dispositivo->marca = $request->marca;
+        $dispositivo->modelo = $request->modelo;
+        $dispositivo->latitud = $request->latitud;
+        $dispositivo->longitud = $request->longitud;
+        $dispositivo->img_url = Storage::url($file);
+        $dispositivo->comentario = $request->comentario;
+        $dispositivo->torre_id = $request->torre_id;
+        $dispositivo->estado = $request->estado;
+        $dispositivo->save();
+        return redirect()->route('torres.show', $request->torre_id)->with('success', 'Dispositivo creado correctamente');
 
     }
 
@@ -89,7 +86,7 @@ class DispositivoController extends Controller
      */
     public function edit(Dispositivo $dispositivo)
     {
-        return view('dispositivos.edit',compact('dispositivo'));
+        return view('dispositivos.edit', compact('dispositivo'));
     }
 
     /**
@@ -101,33 +98,34 @@ class DispositivoController extends Controller
      */
     public function update(UpdateDispositivoRequest $request, Dispositivo $dispositivo)
     {
-          /* verificar si existe una imagen en el request para proceder eliminar el que esta en la base de datos  */
-          if($request->hasFile('img_url')){
+        /* verificar si existe una imagen en el request para proceder eliminar el que esta en la base de datos  */
+        if ($request->hasFile('img_url')) {
             $file = $request->file('img_url');
 
             /* guardar la imagen en el storage */
-           $file =  Storage::disk('public')->put('images/', $file);
+            $file = Storage::disk('public')->put('images/', $file);
 
             /* eliminar la imagen anterior */
-            Storage::delete('public/images/'.$dispositivo->img_url);
+            /* eliminar el storage/ */
+            Storage::delete('public/' .str_replace('/storage/', '', $dispositivo->img_url));
             $dispositivo->img_url = Storage::url($file);
-          }
-            $dispositivo->tipo = $request->tipo;
-            $dispositivo->hostname = $request->hostname;
-            $dispositivo->ip = $request->ip;
-            $dispositivo->usuario = $request->usuario;
-            $dispositivo->password = $request->password;
-            $dispositivo->ssid = $request->ssid;
-            $dispositivo->numero_de_cliente = $request->numero_de_cliente;
-            $dispositivo->marca = $request->marca;
-            $dispositivo->modelo = $request->modelo;
-            $dispositivo->latitud = $request->latitud;
-            $dispositivo->longitud = $request->longitud;
-            $dispositivo->comentario = $request->comentario;
-            $dispositivo->estado = $request->estado;
+        }
+        $dispositivo->tipo = $request->tipo;
+        $dispositivo->hostname = $request->hostname;
+        $dispositivo->ip = $request->ip;
+        $dispositivo->usuario = $request->usuario;
+        $dispositivo->password = $request->password;
+        $dispositivo->ssid = $request->ssid;
+        $dispositivo->numero_de_cliente = $request->numero_de_cliente;
+        $dispositivo->marca = $request->marca;
+        $dispositivo->modelo = $request->modelo;
+        $dispositivo->latitud = $request->latitud;
+        $dispositivo->longitud = $request->longitud;
+        $dispositivo->comentario = $request->comentario;
+        $dispositivo->estado = $request->estado;
 
-            $dispositivo->save();
-            return redirect()->route('torres.show',$dispositivo->torre_id)->with('success','Dispositivo actualizado correctamente');
+        $dispositivo->save();
+        return redirect()->route('torres.show', $dispositivo->torre_id)->with('success', 'Dispositivo actualizado correctamente');
     }
 
     /**
@@ -143,23 +141,23 @@ class DispositivoController extends Controller
 
     public function obtenerDispositivos($id)
     {
-        $dispositivos = Dispositivo::where('torre_id',$id)->get();
+        $dispositivos = Dispositivo::where('torre_id', $id)->get();
         //dd($dispositivos);
         return datatables()->of($dispositivos)
-        ->addColumn('acciones', function($dispositivo){
-            /* ver que muestre un modal de vista */
-            $button = '<a href="javascript:void(0)" data-toggle="modal" data-target="#viewDispositivoModal" data-id="'.$dispositivo->id.'" class="view btn btn-info btn-sm">Ver</a>';
-            $button .= '&nbsp;&nbsp;&nbsp;<a href="/dispositivos/'.$dispositivo->id.'/edit" class="edit btn btn-primary btn-sm">Editar</a>';
-            $button .= '&nbsp;&nbsp;&nbsp;<button type="button" name="delete" id="'.$dispositivo->id.'" class="delete btn btn-danger btn-sm">Eliminar</button>';
-            return $button;
-        })
+            ->addColumn('acciones', function ($dispositivo) {
+                /* ver que muestre un modal de vista */
+                $button = '<a href="javascript:void(0)" data-toggle="modal" data-target="#viewDispositivoModal" data-id="' . $dispositivo->id . '" class="view btn btn-info btn-sm">Ver</a>';
+                $button .= '&nbsp;&nbsp;&nbsp;<a href="/dispositivos/' . $dispositivo->id . '/edit" class="edit btn btn-primary btn-sm">Editar</a>';
+                $button .= '&nbsp;&nbsp;&nbsp;<button type="button" name="delete" id="' . $dispositivo->id . '" class="delete btn btn-danger btn-sm">Eliminar</button>';
+                return $button;
+            })
         /* addcolumn ubicacion */
-        ->addColumn('ubicacion',function($dispositivo){
-            return "<a href='https://www.google.com/maps?q=".$dispositivo->latitud.",".$dispositivo->longitud."' target='_blank'>Ver Ubicacion</a>";
-        })
-        ->rawColumns(['acciones','ubicacion'])
+            ->addColumn('ubicacion', function ($dispositivo) {
+                return "<a href='https://www.google.com/maps?q=" . $dispositivo->latitud . "," . $dispositivo->longitud . "' target='_blank'>Ver Ubicacion</a>";
+            })
+            ->rawColumns(['acciones', 'ubicacion'])
 
-        ->toJson();
+            ->toJson();
 
     }
 }
