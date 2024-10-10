90333523654d4e877737b8252c093cad9f65906b
leonardo maldonado <lrmaldo@gmail.com>
2024-10-10 10:23:49 -0600

    databaseseeder registro

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+       //  \App\Models\User::factory(1000)->create();
+        User::create([
+            'name' => 'admin',
+            'email' => 'leonardo.maldonado@sattlink.com',
+            'password' => bcrypt('12345678')
+        ]);
+
     }
 }
